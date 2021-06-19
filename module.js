bdd7344c4f4bb207e7c81325f3b25f951a69adf9
jro_k <67063704+jr-ok@users.noreply.github.com>
2021-06-19 11:08:29 -0400

    Initial Commit

---
+++ module.js
@@ -0,0 +1,11 @@
+exports.printMsg = function() {
+  console.log("this works :D4");
+}
+
+exports.help = function() {
+    console.log("This is a message from the demo package");
+  }
+
+exports.printMsg = function() {
+    console.log("This is a message from the demo package");
+  }
