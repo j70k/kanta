f9d5b263872bb3b6812925aa4e77f1b3ca95302d
JrOK <67063704+jr-ok@users.noreply.github.com>
2021-09-11 06:25:16 -0700

    Update functions.js

--- modules/functions.js
+++ modules/functions.js
@@ -1,15 +1,20 @@
+/* functions.js - jr-ok/kanta 
+testing module 
+kanta/modules/functions.js
+*/
+
 class FunctionTest {
     constructor() {
 
 module.exports = {
     func1: {
         updateNewFunc: () => {
-            console.log("func1... damn!")
+            console.log("func1 be here")
         }
     },
     func2: {
         updateNewFunc: () => {
-            console.log("func2 man")
+            console.log("func2 be here 2")
         }
     }
 }
